5f6946061b7be2cc2b6c7686b4b257900732a66f
pyon28 <148950239+pyon28@users.noreply.github.com>
2024-03-03 21:07:20 +0900

    修正

--- MisoProject/MisoProject/urls.py
+++ MisoProject/MisoProject/urls.py
@@ -7,6 +7,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
+    
     
 ]  
 
